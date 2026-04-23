83789a03c46498258af518abc364c6809b4b46b2
MarkoAlma <markogyapjas@gmail.com>
2026-04-24 00:23:45 +0200

    sound redesigned, music added

--- src/ai_components/ImagePanel.jsx
+++ src/ai_components/ImagePanel.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ImageGenerator from '../components/image_studio/ImageGenerator';
 
 export default function ImagePanel(props) {
+
   return <ImageGenerator {...props} />;
 }
